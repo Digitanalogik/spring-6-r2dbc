1e71878f3a7f6dc49c53a8c449fe229aad8fd754
Tatu Soininen <taatusti@gmail.com>
2023-09-07 10:37:16 +0300

    MapStruct Mapper to convert between DTO and entity classes

---
+++ src/main/java/fi/tatu/spring6r2dbc/domain/BeerController.java
@@ -0,0 +1,20 @@
+package fi.tatu.spring6r2dbc.domain;
+
+import fi.tatu.spring6r2dbc.model.BeerDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class BeerController {
+
+    public static final String BEER_PATH = "/api/v2/beer";
+
+    @GetMapping(BEER_PATH)
+    Flux<BeerDto> listBeers() {
+        BeerDto beer1 = BeerDto.builder().id(1).build();
+        BeerDto beer2 = BeerDto.builder().id(2).build();
+        return Flux.just(beer1, beer2);
+    }
+
+}
---
+++ src/main/java/fi/tatu/spring6r2dbc/model/BeerDto.java
@@ -0,0 +1,24 @@
+package fi.tatu.spring6r2dbc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
