1a96485f82f1229254c7a4d7812f84fa63e49bdd
Tatu Soininen <taatusti@gmail.com>
2023-09-07 11:03:42 +0300

    List Flux from database

--- src/main/java/fi/tatu/spring6r2dbc/services/BeerService.java
+++ src/main/java/fi/tatu/spring6r2dbc/services/BeerService.java
@@ -1,2 +1,9 @@
-package fi.tatu.spring6r2dbc.services;public interface BeerService {
+package fi.tatu.spring6r2dbc.services;
+
+import fi.tatu.spring6r2dbc.model.BeerDto;
+import reactor.core.publisher.Flux;
+
+public interface BeerService {
+
+    Flux<BeerDto> listBeers();
 }
--- src/main/java/fi/tatu/spring6r2dbc/services/BeerServiceImpl.java
+++ src/main/java/fi/tatu/spring6r2dbc/services/BeerServiceImpl.java
@@ -1,11 +1,22 @@
 package fi.tatu.spring6r2dbc.services;
 
+import fi.tatu.spring6r2dbc.mappers.BeerMapper;
 import fi.tatu.spring6r2dbc.model.BeerDto;
+import fi.tatu.spring6r2dbc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+@RequiredArgsConstructor
+@Service
 public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
     @Override
     public Flux<BeerDto> listBeers() {
-        return null;
+        return beerRepository.findAll()
+            .map(beerMapper::beerToBeerDto);
     }
 }
