1a80f5ea782d86b2ab0af098361ae802d023bc57
Tatu Soininen <taatusti@gmail.com>
2023-09-07 11:13:13 +0300

    Use path variable to provide API endpoint to return one object from database

--- src/main/java/fi/tatu/spring6r2dbc/controllers/BeerController.java
+++ src/main/java/fi/tatu/spring6r2dbc/controllers/BeerController.java
@@ -1,23 +1,32 @@
 package fi.tatu.spring6r2dbc.controllers;
 
+import fi.tatu.spring6r2dbc.domain.Beer;
 import fi.tatu.spring6r2dbc.model.BeerDto;
 import fi.tatu.spring6r2dbc.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
 
     @GetMapping(BEER_PATH)
     Flux<BeerDto> listBeers() {
         return beerService.listBeers();
+    }
+
+    @GetMapping(BEER_PATH_ID)
+    Mono<BeerDto> getBeerById(@PathVariable("beerId") Integer beerId) {
+        return beerService.getBeerById(beerId);
     }
 
 }
--- src/main/java/fi/tatu/spring6r2dbc/services/BeerService.java
+++ src/main/java/fi/tatu/spring6r2dbc/services/BeerService.java
@@ -2,8 +2,11 @@ package fi.tatu.spring6r2dbc.services;
 
 import fi.tatu.spring6r2dbc.model.BeerDto;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
 
     Flux<BeerDto> listBeers();
+
+    Mono<BeerDto> getBeerById(Integer beerId);
 }
