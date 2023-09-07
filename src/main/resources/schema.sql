3175aa29a5b94c2e811a42ea731c006294b0b60f
Tatu Soininen <taatusti@gmail.com>
2023-09-07 14:54:21 +0300

    Reduced field size of UPC code

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS beer
     id                  INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     beer_name           VARCHAR(255),
     beer_style          VARCHAR(255),
-    upc                 VARCHAR(255),
+    upc                 VARCHAR(25),
     quantity_on_hand    INTEGER,
     price               NUMERIC(7,2),
     created_date        TIMESTAMP,
