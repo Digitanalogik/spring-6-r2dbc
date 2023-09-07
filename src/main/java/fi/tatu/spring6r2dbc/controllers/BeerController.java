5510294957fb84742b38e3c7d05d6c2896fd0059
Tatu Soininen <taatusti@gmail.com>
2023-09-07 11:06:03 +0300

    Use service layer in the controller

--- src/main/java/fi/tatu/spring6r2dbc/controllers/BeerController.java
+++ src/main/java/fi/tatu/spring6r2dbc/controllers/BeerController.java
@@ -1,20 +1,23 @@
 package fi.tatu.spring6r2dbc.controllers;
 
 import fi.tatu.spring6r2dbc.model.BeerDto;
+import fi.tatu.spring6r2dbc.services.BeerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequiredArgsConstructor
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
 
+    private final BeerService beerService;
+
     @GetMapping(BEER_PATH)
     Flux<BeerDto> listBeers() {
-        BeerDto beer1 = BeerDto.builder().id(1).build();
-        BeerDto beer2 = BeerDto.builder().id(2).build();
-        return Flux.just(beer1, beer2);
+        return beerService.listBeers();
     }
 
 }
