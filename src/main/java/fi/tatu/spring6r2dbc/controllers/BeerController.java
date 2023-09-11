a9b1e1455f97bbb0617e46468bf023c4a5dc1196
Tatu Soininen <taatusti@gmail.com>
2023-09-11 12:02:00 +0300

    Refactor and clean Beer controller

--- src/main/java/fi/tatu/spring6r2dbc/controllers/BeerController.java
+++ src/main/java/fi/tatu/spring6r2dbc/controllers/BeerController.java
@@ -56,19 +56,6 @@ public class BeerController {
     @PutMapping(BEER_PATH_ID)
     Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
                                                   @Validated @RequestBody BeerDto beerDto) {
-
-        // log.info("Controller was called to update Beer (id={})", beerId);
-        /*
-        // Example: fine-grained processing of reactive stream
-        //  - Consume all elements in the sequence (stream)
-        //  - Handle errors, if any
-        //  - React to completion in the final step
-        beerService.getBeerById(beerId)
-            .subscribe( found -> log.info("Found: {}", found.toString()),
-                        error -> log.error(String.valueOf(error)),
-                           () -> log.info("Controller will next call Service to update Beer (id={})", beerId));
-        */
-
         return beerService.updateBeer(beerId, beerDto)
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .map(saveDto -> ResponseEntity.noContent().build());
@@ -77,11 +64,9 @@ public class BeerController {
     @PatchMapping (BEER_PATH_ID)
     Mono<ResponseEntity<Void>> patchExistingBeer(@PathVariable("beerId") Integer beerId,
                                                  @Validated @RequestBody BeerDto beerDto) {
-
-        beerService.patchBeer(beerId, beerDto)
-                .subscribe();
-
-        return Mono.just(ResponseEntity.ok().build());
+        return beerService.patchBeer(beerId, beerDto)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(updatedDto -> ResponseEntity.ok().build());
     }
 
     @DeleteMapping(BEER_PATH_ID)
