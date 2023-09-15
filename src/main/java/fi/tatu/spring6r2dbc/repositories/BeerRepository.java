4b33159efb0cec2ea640558240ce3f4efea2048d
Tatu Soininen <taatusti@gmail.com>
2023-09-15 10:23:48 +0300

    Add description

--- src/main/java/fi/tatu/spring6r2dbc/repositories/BeerRepository.java
+++
@@ -1,7 +0,0 @@
-package fi.tatu.spring6r2dbc.repositories;
-
-import fi.tatu.spring6r2dbc.domain.Beer;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface BeerRepository extends ReactiveCrudRepository<Beer, Integer> {
-}
--- src/main/java/fi/tatu/spring6r2dbc/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package fi.tatu.spring6r2dbc.repositories;
-
-import fi.tatu.spring6r2dbc.domain.Customer;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
-}
